e39ebb4a531a9a721f6a406e7ad07641d58f52b8
Iulian Rotaru <iulian@gnotis.ro>
2021-01-26 12:58:42 +0200

    Add state class to light dimmer interface.

--- src/main/java/js/hera/dev/LightDimmer.java
+++ src/main/java/js/hera/dev/LightDimmer.java
@@ -2,9 +2,37 @@ package js.hera.dev;
 
 public interface LightDimmer extends Device
 {
-  int updateValue(int value);
-  
-  int setValue(int value);
+  State updateValue(int value);
+
+  State setValue(int value);
 
   int getValue();
+
+  State getState();
+
+  public static class State
+  {
+    private boolean running;
+    private Integer value;
+
+    public boolean isRunning()
+    {
+      return running;
+    }
+
+    public void setRunning(boolean running)
+    {
+      this.running = running;
+    }
+
+    public Integer getValue()
+    {
+      return value;
+    }
+
+    public void setValue(Integer value)
+    {
+      this.value = value;
+    }
+  }
 }
